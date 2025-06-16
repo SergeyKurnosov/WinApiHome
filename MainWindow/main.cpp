9a61c8a10917a7897e754791d81ab830703df053
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-06-16 13:56:04 +1000

    H_W_13_06_2025

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,4 +1,5 @@
 #include<Windows.h>
+#include<cstdio>
 
 CONST CHAR g_sz_CLASS_NAME[] = "My First Window";
 
@@ -10,20 +11,20 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	WNDCLASSEX wClass;
 	ZeroMemory(&wClass, sizeof(wClass));
 
-	wClass.style = 0;
-	wClass.cbSize = sizeof(wClass);
-	wClass.cbWndExtra = 0;
-	wClass.cbClsExtra = 0;
+	wClass.style = 0;    // стиль
+	wClass.cbSize = sizeof(wClass); // размер
+	wClass.cbWndExtra = 0; // доп байты после экзэмрляра окна 
+	wClass.cbClsExtra = 0; // доп байты после структуры 
 
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION); // дескриптор значка
+	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION); // дескриптор небольшого значка
+	wClass.hCursor = LoadCursor(NULL, IDC_ARROW); // дескриптор курсора 
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW; // дескрипотр фоновой кисти 
 
-	wClass.hInstance = hInstance;
-	wClass.lpfnWndProc = (WNDPROC)WndProc;
-	wClass.lpszMenuName = NULL;
-	wClass.lpszClassName = g_sz_CLASS_NAME;
+	wClass.hInstance = hInstance; // дескриптор экземпляра
+	wClass.lpfnWndProc = (WNDPROC)WndProc; // указатель на процедуру окна
+	wClass.lpszMenuName = NULL; // указательна имя ресурса меню класса
+	wClass.lpszClassName = g_sz_CLASS_NAME; // указатель на имя класса окна
 
 	if (RegisterClassEx(&wClass) == NULL)
 	{
@@ -31,14 +32,33 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		return 0;
 	}
 	//2///////////////////////////////////////////////////////////
+
+	RECT rect;
+	SystemParametersInfo(SPI_GETWORKAREA, 0, &rect, 0);
+	int screenWidth = rect.right - rect.left;
+	int screenHeight = rect.bottom - rect.top;
+
+	int windowWidth = (int)screenWidth * 0.75;
+	int windowHeight = (int)screenHeight * 0.75;
+
+	int xPos = rect.left + (screenWidth - windowWidth) / 2;
+	int yPos = rect.top + (screenHeight - windowHeight) / 2;
+
+	CONST INT SIZE = 256;
+
+	CHAR sz_message[256] = {};
+	sprintf(sz_message, "Размер: %dx%d, Положение: (%d,%d)", windowWidth, windowHeight, xPos, yPos);
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,
 		g_sz_CLASS_NAME,
-		g_sz_CLASS_NAME,
+		sz_message,
 		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT,
+		xPos, 
+		yPos,
+		windowWidth,
+		windowHeight,
 		NULL,
 		NULL,
 		hInstance,
