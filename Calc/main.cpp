da456e6fe152b39f8392f053bf02279964e7a3a5
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-06-20 23:31:18 +1000

    WS_THICKFRAME & WS_MAXIMIZEBOX removed

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -6,12 +6,6 @@ CONST CHAR g_sz_CLASS_NAME[] = "MyCalc";
 CONST CHAR* g_sz_OPERATIONS[] = { "+","-","*","/" };
 CONST CHAR* g_sz_EDIT[] = { "<-","C","=" };
 
-//CONST CHAR* g_sz_BUTTONS[4][5] =
-//{
-//	{"7","8","9","/","<-"},
-//	{"4","5","6","*","C"},
-//}
-
 //g_i_ - Global Integer
 CONST INT g_i_BUTTON_SIZE = 50;
 CONST INT g_i_INTERVAL = 1;
@@ -25,6 +19,9 @@ CONST INT g_i_DISPLAY_HEIGHT = 22;
 CONST INT g_i_DISPLAY_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
 CONST INT g_i_BUTTON_START_X = g_i_START_X;
 CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL;
+
+CONST INT g_i_WINDOW_WIDTH = g_i_DISPLAY_WIDTH + 2 * g_i_START_X + 16;
+CONST INT g_i_WINDOW_HEIGHT = (g_i_DISPLAY_HEIGHT + g_i_INTERVAL) + g_i_BUTTON_SPACE * 4 + 2 * g_i_START_Y + 24 + 16;
 
 CONST INT g_SIZE = 256;
 
@@ -63,9 +60,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL,
 		g_sz_CLASS_NAME,
 		g_sz_CLASS_NAME,
-		WS_OVERLAPPEDWINDOW,
+	//	WS_OVERLAPPED | WS_SYSMENU /*| WS_THICKFRAME*/  | WS_MINIMIZEBOX | /*WS_MAXIMIZEBOX*/,
+		WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
 		CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT,
+		g_i_WINDOW_WIDTH, g_i_WINDOW_HEIGHT,
 		NULL,
 		NULL,
 		hInstance,
@@ -187,17 +185,22 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		CHAR szDisplay[g_SIZE] = {};
 		CHAR szDigit[2] = {};
 		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
-		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_POINT)
 		{
-			szDigit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
+			if (LOWORD(wParam) == IDC_BUTTON_POINT)
+				szDigit[0] = '.';
+			else
+				szDigit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			SendMessage(hEditDisplay, WM_GETTEXT, g_SIZE, (LPARAM)szDisplay);
 			if (szDisplay[0] == '0' && szDisplay[1] != '.')szDisplay[0] = 0;
+			if (szDigit[0] == '.' && strchr(szDisplay, '.'))break;
+			//
 			strcat(szDisplay, szDigit);
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)szDisplay);
 		}
 
 	}
-		break;
+	break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
